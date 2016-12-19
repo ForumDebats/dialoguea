99fddaa2aecd3fc7e7e58f7d65de6cd94c2e5e31
flintforge <flint@forge.systems>
2016-12-19 10:21:50 +0100

    license MIT/GPL

--- runforever.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-forever --minUptime 1000 --spinSleepTime 1000 -wc node server.js --color
---
+++ settings.js
@@ -0,0 +1,28 @@
+/**
+ * CONFIGURATION FILE
+ */
+
+
+var upload_dir = 'data/'
+
+exports.configuration = {
+
+   APP: 'dialoguea',
+   SITE: 'https://dialoguea.io',
+	SERVER: "localhost:2016",
+	PROTOCOL: "http://",
+	PORT: 2016,
+	DEBUG: false,
+	LOG_FILE:'./logs/server.log', // todo rotations
+	MONGO:'mongodb://localhost/',
+	DB: '',
+	ADMIN_MODULE: __dirname + '/server/views/adminmodule.min.js',
+	UPLOAD_DIR: upload_dir,
+	ABS_UPLOAD_DIR: '' + __dirname + '/public/'+upload_dir,
+	MAX_DLSIZE: 5700000,
+	THUMB: {WIDTH: 250, HEIGHT: 150},
+	GROUP_PUBLIC:'public',
+	ADMIN_GROUP : ['public'], //hide passwords
+	TEMPPOSTTIME: 30 , //seconds
+	MODE_PUBLIC: false
+};
