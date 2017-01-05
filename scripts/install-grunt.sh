01f0aed1b11cccb6e312058305e18e2ef699a143
flintforge <flint@forge.systems>
2017-01-05 09:45:22 +0100

    scripts d'installations

---
+++ scripts/install-grunt.sh
@@ -0,0 +1,8 @@
+cd ..
+
+npm install -g grunt-cli
+npm install grunt --save-dev
+npm install -g grunt-closure-compiler
+npm install grunt-contrib-concat
+wget https://closure-compiler.googlecode.com/files/compiler-20131014.zip
+# cp to /usr/share/java/build/
---
+++ scripts/install-tinyup.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+cd ..
+cd ./public/dist/tinymce/plugins;  ln -sf ../../../../public/plugins/tinymce-plugin/upimage;
+
