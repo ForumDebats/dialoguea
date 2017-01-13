64a46adcf550203fa308072e9ecc31e478584774
flintforge <flint@forge.systems>
2017-01-13 18:19:51 +0100

    public login window

--- public/js/settings.js
+++ public/js/settings.js
@@ -7,6 +7,6 @@ console_dbg=function(){}
 Settings = {
 	default_login_policy: true,
 	storage : "LocalStorage", /*"SessionStorage"*/
-	createAccountAllowed : false,
+	createAccountAllowed : true,
 	postTimeout:4*60*1000 // TODO :: compiler des constantes pour à la fois côté et client et serveur
 }
